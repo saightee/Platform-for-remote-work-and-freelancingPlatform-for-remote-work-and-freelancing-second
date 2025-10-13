85f755afc8415fe20b2bf2878fa1aba99601fe0c
RomanBukshak <113369694+RomanBukshak@users.noreply.github.com>
2025-10-13 10:33:14 +0700

    Правка лимитов и удаление пользователей с новой таблой инвайтов

--- backend/src/application-limits/application-limits.module.ts
+++ backend/src/application-limits/application-limits.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ApplicationLimit } from './application-limit.entity';
 import { ApplicationLimitsService } from './application-limits.service';
+import { JobApplication } from '../job-applications/job-application.entity';
+import { JobPost } from '../job-posts/job-post.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ApplicationLimit])],
+  imports: [TypeOrmModule.forFeature([ApplicationLimit, JobApplication, JobPost])],
   providers: [ApplicationLimitsService],
   exports: [ApplicationLimitsService],
 })
